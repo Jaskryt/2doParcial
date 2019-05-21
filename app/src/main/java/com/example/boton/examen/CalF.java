25a383a46d9b89dc0c68cb99df9a94a90de7f363
Jaskryt <jurassciandycraft@gmail.com>
2019-05-21 18:53:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/boton/examen/CalF.java
@@ -0,0 +1,36 @@
+package com.example.boton.examen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CalF extends AppCompatActivity {
+    EditText text;
+    TextView tot;
+    int aconver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ke_c);
+        try {
+            Button conver = findViewById(R.id.convertir);
+            text = findViewById(R.id.cont);
+            tot = findViewById(R.id.Convertido);
+            conver.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    aconver = Integer.parseInt(text.getText().toString());
+                    tot.setText((String.valueOf(aconver - 273.15)) + "ºC");
+                }
+            });
+        }
+        catch (Exception ex){
+            Toast toast1 = Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_SHORT);
+            toast1.show();
+        }
+    }
+}
